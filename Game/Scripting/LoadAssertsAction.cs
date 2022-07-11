8332119f518bdd94987d7373c4241c19f94e0127
kai <kaidizhang@byui.edu>
2022-07-11 15:57:41 -0600

    added loadassertsaction class

---
+++ Game/Scripting/LoadAssertsAction.cs
@@ -0,0 +1,25 @@
+using Tank.Game.Casting;
+using Tank.Game.Services;
+
+
+namespace Tank.Game.Scripting
+{
+    public class LoadAssetsAction : Action
+    {
+        private AudioService audioService;
+        private VideoService videoService;
+        
+        public LoadAssetsAction(AudioService audioService, VideoService videoService)
+        {
+            this.audioService = audioService;
+            this.videoService = videoService;
+        }
+
+        public void Execute(Cast cast, Script script, ActionCallback callback)
+        {
+            audioService.LoadSounds("Assets/Sounds");
+            videoService.LoadFonts("Assets/Fonts");
+            videoService.LoadImages("Assets/Images");
+        }
+    }
+}

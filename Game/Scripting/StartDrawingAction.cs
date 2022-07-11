1ca7effee07eb0d514d331cb1593fa8be6cbab36
kai <kaidizhang@byui.edu>
2022-07-11 15:47:38 -0600

    added StartDrawingAction class

---
+++ Game/Scripting/StartDrawingAction.cs
@@ -0,0 +1,21 @@
+using Tank.Game.Casting;
+using Tank.Game.Services;
+
+
+namespace Tank.Game.Scripting
+{
+    public class StartDrawingAction : Action
+    {
+        private VideoService videoService;
+        
+        public StartDrawingAction(VideoService videoService)
+        {
+            this.videoService = videoService;
+        }
+
+        public void Execute(Cast cast, Script script, ActionCallback callback)
+        {
+            videoService.ClearBuffer();
+        }
+    }
+}
